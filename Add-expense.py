13195228aa40b7c609dad530975311f1c2629715
roweldulu12-hub <roweldulu12@gmail.com>
2026-08-16 13:30:04 +0000

    inedit ko onti yung add expense

--- Add-expense.py
+++ Add-expense.py
@@ -1,13 +1,14 @@
 budget = 0
 transpo = 0
 util = 0
-insurance = 0 
+insurance = 0
 food = 0
-def Add(budget, transpo, util, insurance, food):
-    print (int(input("Add Budget: ")))
-    print (int(input("Transportation expense(Gas and travel expense): ")))
-    print (int(input("Utilities expense(Subscription, Bills, etc.): ")))
-    print (int(input("Insurance expense(Car insurrance, Health insurance, etc): ")))
-    print (int(input("Food expense(i.e Groceries): ")))
 
-Add(budget, transpo, util, insurance, food)
+def Add(budget, transpo, util, insurance, food):
+    budget = int(input("Add Budget: "))
+    transpo = int(input("Transportation expense (Gas and travel expense): "))
+    util = int(input("Utilities expense (Subscription, Bills, etc.): "))
+    insurance = int(input("Insurance expense (Car insurance, Health insurance, etc.): "))
+    food = int(input("Food expense (i.e. Groceries): "))
+
+    return budget, transpo, util, insurance, food
